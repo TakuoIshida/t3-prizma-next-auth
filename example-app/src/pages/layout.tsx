febd4a16c3b7d34b72fcbcb451564d96445c021e
Tishida <archersparadox.100@gmail.com>
2024-01-09 15:06:01 +0900

    init

---
+++ example-app/src/pages/layout.tsx
@@ -0,0 +1,25 @@
+import { type Session } from "next-auth";
+import { SessionProvider } from "next-auth/react";
+import { type AppType } from "next/app";
+
+import { api } from "~/utils/api";
+
+import "~/styles/globals.css";
+
+import { Metadata } from "next";
+export const metadata: Metadata = {
+  title: "T3-app",
+};
+
+const MyApp: AppType<{ session: Session | null }> = ({
+  Component,
+  pageProps: { session, ...pageProps },
+}) => {
+  return (
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
+};
+
+export default api.withTRPC(MyApp);
